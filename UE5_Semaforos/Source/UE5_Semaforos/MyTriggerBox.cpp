363cfda6de2f2b4dd370270f0aab15985f65ac02
Alonso García <167032274+alonsoggdev@users.noreply.github.com>
2024-05-01 09:55:29 +0200

    Bouncing v2

--- UE5_Semaforos/Source/UE5_Semaforos/MyTriggerBox.cpp
+++ UE5_Semaforos/Source/UE5_Semaforos/MyTriggerBox.cpp
@@ -30,25 +30,23 @@ void AMyTriggerBox::Tick(float DeltaTime)
 
 void AMyTriggerBox::NotifyActorBeginOverlap(AActor* OtherActor)
 {
-	if (GEngine != nullptr)
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Hello from Codded Trigger to ") + OtherActor->GetName());
+	//if (GEngine != nullptr) GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Hello from Codded Trigger to ") + OtherActor->GetName());
 
 	Car = Cast<ACar>(OtherActor);
 
 	if (Car) {
 
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Cast Pedestrian ") + OtherActor->GetName());
+		//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Cast Car ") + OtherActor->GetName());
 
 		Car->direction *= -1;
 
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Direction: %s"), *Car->direction.ToString()));
+		//GEngine->AddOnScreenDebugMessage(-1, 5, FColor::Red, FString::Printf(TEXT("Direction: %s"), *Car->direction.ToString()));
 	}
 
 }
 
 void AMyTriggerBox::NotifyActorEndOverlap(AActor* OtherActor)
 {
-	if (GEngine != nullptr)
-		GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Bye from Codded Trigger to ") + OtherActor->GetName());
+	//if (GEngine != nullptr) GEngine->AddOnScreenDebugMessage(-1, 5, FColor::White, TEXT("Bye from Codded Trigger to ") + OtherActor->GetName());
 }
 
